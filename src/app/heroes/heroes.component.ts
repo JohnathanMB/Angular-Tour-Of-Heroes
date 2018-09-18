6f817c96a12523966633e1246b14682ea2f24859
MonitorVDO Docencia <monitorvdo10@udea.local>
2018-09-18 17:04:24 -0500

    Service and messsage -step 6-

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,33 +1,44 @@
 import { Component, OnInit } from '@angular/core';
-import { Hero } from '../Hero';
-import { HEROES } from '../Mock-Heroes'
-
+ 
+import { Hero } from '../hero';
+import { HeroService } from '../hero.service';
+ 
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
 })
+
 export class HeroesComponent implements OnInit {
 
+  /*
   // Crea una instancia de **Hero** como un Json
   hero: Hero = {
     id : 1,
     name : 'Windstorm'
   };
-
-  //importar varios heroes
-  heroes = HEROES;
+  */
 
   //paso de seleccionar hero
   selectedHero: Hero;
-  
-  onSelect(hero: Hero):void{
+
+  //importar varios heroes
+  heroes: Hero[];
+
+
+  constructor(private heroService: HeroService) { }
+
+  ngOnInit() {
+    this.getHeroes();
+  }
+
+  onSelect(hero: Hero): void {
     this.selectedHero = hero;
   }
 
-  constructor() { }
-
-  ngOnInit() {
+  getHeroes(): void {
+    this.heroService.getHeroes().subscribe(
+      heroes => this.heroes = heroes
+    );
   }
-
 }
